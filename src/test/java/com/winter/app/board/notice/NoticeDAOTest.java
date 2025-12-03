4eb3aa3f021862acff28474969b815ed9e7cac6b
Daeki <kimdaeki@gmail.com>
2025-12-03 12:43:48 +0900

    aop1

--- src/test/java/com/winter/app/board/notice/NoticeDAOTest.java
+++ src/test/java/com/winter/app/board/notice/NoticeDAOTest.java
@@ -5,19 +5,23 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
+@Transactional
 class NoticeDAOTest {
 
 	@Autowired
 	private NoticeDAO noticeDAO;
 	
 	@Test
+	@Rollback(false)
 	void testAdd()throws Exception{
 		for(int i=0;i<120;i++) {
 			NoticeDTO noticeDTO = new NoticeDTO();
 			noticeDTO.setBoardTitle("title"+i);
 			noticeDTO.setBoardWriter("writer"+i);
-			noticeDTO.setBoardContets("contents"+i);
+			noticeDTO.setBoardContents("contents"+i);
 			noticeDAO.add(noticeDTO);
 			if(i%10==0) {
 				Thread.sleep(500);
