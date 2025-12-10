d96d02a3dcdc6aa2c81fa1eb0417dce5fada7e1f
Daeki <kimdaeki@gmail.com>
2025-12-10 17:52:06 +0900

    sns2

--- src/main/java/com/winter/app/users/UserDTO.java
+++ src/main/java/com/winter/app/users/UserDTO.java
@@ -4,10 +4,12 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -19,7 +21,7 @@ import lombok.ToString;
 @Setter
 @Getter
 @ToString
-public class UserDTO implements UserDetails{
+public class UserDTO implements UserDetails, OAuth2User{
 	
 	@NotBlank(groups = {RegisterGroup.class})
 	private String username;
@@ -43,11 +45,15 @@ public class UserDTO implements UserDetails{
 	private UserFileDTO userFileDTO;
 	
 	private List<RoleDTO> roleDTOs;
-	
+
+	//UserDetail
 	private boolean accountNonExpired;
 	private boolean accountNonLocked;
 	private boolean credentialsNonExpired;
 	private boolean enabled;
+	
+	
+	
 	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -61,6 +67,13 @@ public class UserDTO implements UserDetails{
 		
 		return list;
 	}
+
+
+
+	//OAuth2User
+	private Map<String, Object> attributes;
+	private String sns;
+	
 
 	
 	
