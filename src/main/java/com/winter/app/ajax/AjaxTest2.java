6bb883ed60372482b01cdf47d52edbac9669dd44
Daeki <kimdaeki@gmail.com>
2025-12-11 10:50:19 +0900

    ajax1

---
+++ src/main/java/com/winter/app/ajax/AjaxTest2.java
@@ -0,0 +1,16 @@
+package com.winter.app.ajax;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class AjaxTest2 {
+	
+	public void t1()throws Exception{
+		
+		
+	}
+
+}
